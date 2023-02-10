1ca6bce80620b310ed62789bc1b4ecad829617ca
chq09093112 <41619086+chq09093112@users.noreply.github.com>
2023-02-10 14:57:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_weekdays():
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
+  weather = res[b'data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
